ed981b16ac615f29d3f726c9063bfe7c7b1ea700
DanielY121 <96324127+DanielY121@users.noreply.github.com>
2021-12-18 13:52:28 +0800

    MakeCode 專案的初始檔案

---
+++ main.ts
@@ -0,0 +1,90 @@
+input.onButtonPressed(Button.A, function () {
+    Player.change(LedSpriteProperty.X, -1)
+})
+input.onButtonPressed(Button.AB, function () {
+    Bullet = game.createSprite(Player.get(LedSpriteProperty.X), 3)
+    basic.pause(250)
+    for (let index = 0; index < 5; index++) {
+        if (Bullet.isTouching(Enemy)) {
+            Enemy.delete()
+            game.addScore(1)
+            Enemy = game.createSprite(0, 0)
+            Player.delete()
+            Bullet.delete()
+            Bullet_from_enemy.delete()
+            Player = game.createSprite(2, 4)
+        } else if (Bullet.isTouching(Bullet_from_enemy)) {
+            Bullet_from_enemy.delete()
+            Bullet.delete()
+        } else {
+            Bullet.change(LedSpriteProperty.Y, -1)
+            basic.pause(250)
+        }
+    }
+    Bullet.delete()
+})
+input.onButtonPressed(Button.B, function () {
+    Player.change(LedSpriteProperty.X, 1)
+})
+let Enemy_speed = 0
+let sprite = 0
+let Bullet_from_enemy: game.LedSprite = null
+let Bullet: game.LedSprite = null
+let Enemy: game.LedSprite = null
+let Player: game.LedSprite = null
+Player = game.createSprite(2, 4)
+Enemy = game.createSprite(0, 0)
+basic.forever(function () {
+    for (let index = 0; index < 4; index++) {
+        sprite = randint(0, 1)
+        if (sprite == 0) {
+            Enemy.change(LedSpriteProperty.X, 1)
+            Enemy_speed = randint(0, 2)
+            if (Enemy_speed == 0) {
+                basic.pause(100)
+            } else if (Enemy_speed == 1) {
+                basic.pause(250)
+            } else {
+                basic.pause(500)
+            }
+        } else {
+            Bullet_from_enemy = game.createSprite(Enemy.get(LedSpriteProperty.X), 1)
+            basic.pause(500)
+            Enemy.change(LedSpriteProperty.X, 1)
+            for (let index = 0; index < 4; index++) {
+                Bullet_from_enemy.change(LedSpriteProperty.Y, 1)
+                basic.pause(250)
+                if (Bullet_from_enemy.isTouching(Player)) {
+                    game.gameOver()
+                }
+            }
+            Bullet_from_enemy.delete()
+        }
+    }
+    for (let index = 0; index < 4; index++) {
+        sprite = randint(0, 1)
+        if (sprite == 0) {
+            Enemy.change(LedSpriteProperty.X, -1)
+            Enemy_speed = randint(0, 2)
+            if (Enemy_speed == 0) {
+                basic.pause(100)
+            } else if (Enemy_speed == 1) {
+                basic.pause(250)
+            } else {
+                basic.pause(500)
+            }
+        } else {
+            Bullet_from_enemy = game.createSprite(Enemy.get(LedSpriteProperty.X), 1)
+            basic.pause(500)
+            Enemy.change(LedSpriteProperty.X, -1)
+            for (let index = 0; index < 4; index++) {
+                Bullet_from_enemy.change(LedSpriteProperty.Y, 1)
+                basic.pause(250)
+                if (Bullet_from_enemy.isTouching(Player)) {
+                    game.gameOver()
+                }
+            }
+            Bullet_from_enemy.delete()
+        }
+    }
+})
